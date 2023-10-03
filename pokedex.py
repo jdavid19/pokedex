86b4a83aa3c31e9daaa9432f9209377446899ef3
John David <jdavidce19@gmail.com>
2023-10-04 02:33:36 +0800

    Challenge 2

--- pokedex.py
+++ pokedex.py
@@ -3,10 +3,21 @@ import sys
 
 def search_pokemon(name):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/")
-    #response = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format(name))
     pokemon = response.json()
-    print("Name: "+pokemon["name"].capitalize())
+    get_data(pokemon)
+
+def get_data(pokemon):
+     pokemon_name(pokemon)
+     pokemon_id(pokemon)
+     pokemon_exp(pokemon)
+
+def pokemon_name(pokemon):
+        print("Name: "+pokemon["name"].capitalize())
+
+def pokemon_id(pokemon):
     print("ID: "+(str(pokemon["id"])))
+
+def pokemon_exp(pokemon):
     print("Base XP: "+(str(pokemon["base_experience"])))
 
 if __name__ == "__main__":

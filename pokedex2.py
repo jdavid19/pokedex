b8ef4faf9906d6f044160987de1984f7fb45c139
John David <jdavidce19@gmail.com>
2023-10-06 16:06:41 +0800

    adding comments to each function

--- pokedex2.py
+++ pokedex2.py
@@ -1,5 +1,6 @@
 import requests
 
+# Input function
 def input_name():
     i = 0
     while i < 6:
@@ -8,29 +9,35 @@ def input_name():
             search_pokemon(name)
             i += 1
         except:
-            print("Pokemon name does not exist. Try again.")
+            print("Pokemon does not exist. Try again.")
             print("----------------------------")
 
+# Search Pokemon by name
 def search_pokemon(name):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/")
     pokemon = response.json()
     get_data(pokemon)
 
+# Get the response by assigning a value into the print functions
 def get_data(pokemon):
     pokemon_name(pokemon)
     pokemon_hp(pokemon)
     pokemon_attack(pokemon)
     pokemon_item(pokemon)
 
+# Print Pokemon Name
 def pokemon_name(pokemon):
     print("Name:",pokemon["name"].capitalize())
 
+# Print Pokemon Health
 def pokemon_hp(pokemon):
     print("HP:",str(pokemon["stats"][0]["base_stat"]))
 
+# Print Pokemon Attack
 def pokemon_attack(pokemon):
     print("Attacks:",pokemon["moves"][0]["move"]["name"].title())
 
+# Print Pokemon Held Items
 def pokemon_item(pokemon):
     try:
         print("Held Items:",pokemon["held_items"][0]["item"]["name"].title())
